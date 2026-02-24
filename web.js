b6d47374f871a9cd1f5a64b4d0c5a0e7f7aec35f
Charan <gdchcharan@gmail.com>
2026-02-25 02:30:22 +0530

    Home page created

--- web.js
+++ web.js
@@ -1,6 +1,22 @@
+// const { link } = require("node:fs");
+// Boolean id = false ;
+// Boolean pass= false;
 //git // Password toggle
 const togglePassword = document.getElementById("togglePassword");
 const passwordInput = document.getElementById("password");
+const submit= document.getElementById("submit");  
+
+submit.addEventListener("click",(e)=>{
+  e.preventDefault(); // Prevent default form submission behavior
+  if(validateForm()){
+  window.location.href = "home.html";}
+  else{
+     alert("Please fill in all fields correctly."); 
+     console.log("enter a crt passwrd");
+  }
+} );
+
+
 
 togglePassword.addEventListener("click", () => {
   if (passwordInput.type === "password") {
@@ -36,7 +52,38 @@ userType.addEventListener("change", () => {
 });
 
 // Submit (temporary)
-document.getElementById("login-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  alert("Backend not connected");
-});
+// document.getElementById("login-form").addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   alert("Backend not connected");
+// });
+
+
+
+function validateForm() {
+  const idval = userId.value.trim();
+  const password = document.getElementById("password").value.trim();
+  
+  if (idval === "" || password === "") {
+    alert("Please enter your ID and password.");
+    return false;
+  }
+  if(userType.value ==="student" && idval.length !==7) {
+    alert("Please enter a valid ID .");
+    return false;
+  }
+  if(userType.value ==="staff" && idval.length< 8){
+    alert("Please enter a valid ID .");
+    return false;
+
+  }
+  if(userType.value ==="admin" && idval.length< 5){
+    alert("Please enter a valid ID .");
+    return false;
+  }
+  //if(userId.length < 7) {
+  //   alert("Please enter a valid ID .");
+  //   return false;
+  // }
+
+  return true;
+}
